2738ab0211d9b620a352dbe3251e35260f84edd3
lilather <assurestudios@gmail.com>
2022-04-27 18:57:44 -0500

    4/27

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-const { src, dest } = require('gulp');
+
+const {
+  src,
+  dest
+} = require('gulp');
 const gulp = require('gulp');
 var browserSync = require("browser-sync").create();
 const plugins = require('gulp-load-plugins')();
 const sass = require('gulp-sass')(require('sass'));
-
-
+const imagemin = import('gulp-imagemin')
 function pug() {
   return src('./pug/**/*.pug').pipe(plugins.pug()).pipe(dest('./'))
 }
@@ -13,11 +16,20 @@ function pug_portfolio() {
   return src('./pug/portfolio/*.pug').pipe(plugins.pug()).pipe(dest('./portfolio'))
 }
 
- function scss() {
+function scss() {
   // body omitted
   return src('./sass/*.scss').pipe(sass()).pipe(dest('./css/'))
 }
 
+function webP() {
+  return src('./img/**/*')
+    .pipe(plugins.webp())
+    .pipe(dest('./img/'))
+}
+
+function imgMin(){
+  return src('/img/*').pipe(imagemin).pipe(dest('/img'))
+}
 /*
 function css() {
   // body omitted
@@ -25,11 +37,11 @@ function css() {
   .pipe((plugins.cleanCss())).pipe(plugins.purgecss({ content: ["./prod/*.html"] })).pipe(dest('./prod/css/')).pipe(plugins.gzip()).pipe(gulp.dest("./prod/css/"));
 }
 
-function js(){ 
+function js(){
   return src("./dev/js/*.js").pipe(plugins.concat('bundle.js')).pipe(plugins.babel({
     presets: ['@babel/env']
 })).pipe(plugins.uglify()).pipe(gulp.dest('./prod/js')).pipe(plugins.gzip()).pipe(gulp.dest("./prod/js/"));
-} 
+}
 
 function html(){
 return src('./dev/*html').pipe(plugins.htmlclean()).pipe(gulp.dest('./prod/'))
@@ -58,21 +70,20 @@ function sitemap(){
 */
 
 
-function watchDev(done){
+function watchDev(done) {
   browserSync.init({
     server: {
       baseDir: '.'
-    }    
+    }
   });
   plugins.watch('./sass/*.scss', scss).on('change', browserSync.reload);;
   plugins.watch('./pug/*.pug', pug).on('change', browserSync.reload);
-done();
-  }
+  done();
+}
 
-
-  
-exports.watch= watchDev;
-exports.pp =pug_portfolio;
- exports.pug=pug;
- exports.sass=scss;
- 
+exports.imgMin=imgMin;
+exports.webP=webP;
+exports.watch = watchDev;
+exports.pp = pug_portfolio;
+exports.pug = pug;
+exports.sass = scss;
